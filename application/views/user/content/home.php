73b6429a85c0531d93544af1f0bb35c8c280fce0
ahlamls <fulldoang@gmail.com>
2020-06-22 13:13:06 +0700

    asese

--- application/views/user/content/home.php
+++ application/views/user/content/home.php
@@ -5,7 +5,9 @@
 Selamat Datang di Website WRB Online ,  Kami membantu segala kebutuhan pelanggan yang berhubungan dengan layanan catering , mulai catering harian sampai acara-acara penting lainnya
 <br><br>
 <b>Batas Pemesanan Jam 9 Malam<br>Pesanan Diantar mulai dari jam 11:30</b>
-<p>Jika Pesan Diatas Jam 9 Pagi maka hanya bisa memilih menu bertanda ready stock</div>
+<p>Jika Pesan Diatas Jam 9 Pagi maka hanya bisa memilih menu bertanda ready stock<br>
+
+Untuk Daerah Antapani dan Arcamanik Gratis Ongkir . Untuk Daerah Lain Menggunakan Gosend</div>
 <div class="alert alert-success" role="alert">
 <b>Promo</b> Setiap Pembelian Menggunakan Situs WRB Online akan mendapatkan salah satu reward berikut : Kerupuk 1 Toros , Aneka Snack atau Sambal Dadak . <br>Ketentuan : Promo Berlaku Selama Barang Tersedia
 </div>
